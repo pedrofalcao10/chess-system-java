68c9c25d9a5f646d8e971b251fd2c332c21ee063
Pedro Falcão <falcao.comp@gmail.com>
2024-03-25 12:47:26 -0300

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -12,6 +12,11 @@ public class Pawn extends ChessPiece {
     }
 
     @Override
+    public String toString() {
+        return "P";
+    }
+
+    @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -71,10 +76,5 @@ public class Pawn extends ChessPiece {
             }
         }
         return mat;
-    }
-
-    @Override
-    public String toString() {
-        return "p";
     }
 }
