8717414b76725ed3bd6eb1779e81be04fec868f0
Pedro Falcão <falcao.comp@gmail.com>
2024-03-27 20:35:57 -0300

    Castling

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -2,10 +2,12 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Knight extends ChessPiece {
+
     public Knight(Board board, Color color) {
         super(board, color);
     }
